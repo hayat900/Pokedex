d8c38cbe3fbd469f19a9d756ec6b44981efe658b
hayat900 <dishadsouza2000@gmail.com>
2021-05-15 17:58:52 +0530

    performed required parsing

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,8 +1,60 @@
 import React from 'react';
+import { pokemonData } from '../../data/pokemonData';
 import Pokedx from '../Pokedx/pokedx';
 import PokeSearchResult from '../Poksearchresults/poksearch';
+import {
+    PokemonSchema,
+    PokemonSpritesSchema,
+    UnpatchedPokemonSchema,
+} from "../../types/PokemonSchema";
 import './App.css';
-class App extends React.Component{
+interface Appstate{
+    searchField:string;
+    allPokemons:PokemonSchema[];
+    selectedPokemon:PokemonSchema | undefined;
+    searchedPokemons:PokemonSchema[];
+}
+class App extends React.Component<any,Appstate>{
+    state={
+        searchField:"",
+    allPokemons:[],
+    selectedPokemon:undefined,
+    searchedPokemons:[]
+    }
+    patchPokemonData = (pokemons: UnpatchedPokemonSchema[]) => {
+        const patchedPokemons = pokemons.map((pokemon) => {
+            let parsedSprites: PokemonSpritesSchema = {
+                normal: undefined,
+                animated: undefined,
+            };
+
+            try {
+                parsedSprites = pokemon.sprites && JSON.parse(pokemon.sprites);
+            } catch (e) {
+                console.log("Exception while parsing sprites: ", e);
+            }
+
+            const patchedPokemon: PokemonSchema = {
+                ...pokemon,                      //for each pokemon in unpatchedpokemonschema set speites
+                                                 //to paresedsprites
+                sprites: parsedSprites,
+            };
+
+            return patchedPokemon;
+        });
+        return patchedPokemons;
+    }
+    componentDidMount(){
+        const patchedPokemons: PokemonSchema[] = this.patchPokemonData( //this method is present above didmount
+            pokemonData  //this is retrned from data folder
+        );
+console.log(patchedPokemons);
+        // Update the state with patched pokemons
+        this.setState({
+            allPokemons: patchedPokemons,
+            searchedPokemons: patchedPokemons,
+        });
+    }
     render()
     {
         return (
